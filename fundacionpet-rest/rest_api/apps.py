98f508ac47eb2fd571435d059814d1f48bb25c69
marcopezoa2 <ma.pezoa@duocuc.cl>
2023-07-24 17:11:42 -0400

    upload

---
+++ fundacionpet-rest/rest_api/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+"""
+class RestProductoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rest_producto'
+"""
+class RestApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rest_api'
